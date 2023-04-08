013ee620b0b22fc3b46c6dbd5d8f3ceac0719487
leo78913 <leo3758@riseup.net>
2023-04-08 20:54:03 -0300

    fix stair orientation

--- src/game/map.ts
+++ src/game/map.ts
@@ -281,7 +281,7 @@ export class Map {
         this.game.staticObjects.add(new Structure(this.game, typeString, position, orientation, layerObjIds));
 
         for (const stairData of structureData.stairs) {
-            this.game.stairs.add(new Stair(position, orientation, stairData));
+            this.game.stairs.add(new Stair(position, orientation ? orientation : 0, stairData));
         }
     }
 
