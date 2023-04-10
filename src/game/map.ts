bff74fd0f2d8e89094baf33407116bad7674d218
leo78913 <leo3758@riseup.net>
2023-04-09 21:10:28 -0300

    fix sliding doors and refactor the door code a bit

--- src/game/map.ts
+++ src/game/map.ts
@@ -433,7 +433,8 @@ export class Map {
             parentBuilding,
             bunkerWall
         );
-        this.game.staticObjects.add(obstacle);
+        if(obstacle.door?.slideToOpen) this.game.dynamicObjects.add(obstacle)
+        else this.game.staticObjects.add(obstacle);
         return obstacle;
     }
 
